d0ce57c2ebc2e130c4ebb75105e82fe87a12f24b
Justinas Simas <simjustinas@gmail.com>
2023-05-29 19:42:08 +0100

    still need to render credentials' errors but now changing tasks

--- owly-backend/utils/AppError.js
+++ owly-backend/utils/AppError.js
@@ -1,6 +1,8 @@
 class AppError extends Error {
   constructor(message, statusCode) {
     super(message);
+
+    this.statusCode = statusCode;
     this.success = `${statusCode}`.startsWith("2");
 
     Error.captureStackTrace(this, this.constructor);
